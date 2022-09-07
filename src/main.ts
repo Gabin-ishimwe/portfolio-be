bda05c5fef8e338c496b073f6f824b2a7938ac1e
gabin-AF <gabin@augmented-future.com>
2022-09-07 14:02:08 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { configs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
